56489560d53a25ec4aa04179339ffde0decf610b
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-09-18 09:02:48 +0300

    PAtients models create and home pages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 "use strict";
 const router = require("express").Router(),
 homeRoutes = require("./homeRoutes"),
-userRoutes = require("./userRoutes");
+userRoutes = require("./userRoutes"),
+patientRoutes = require("./patientRoutes");
 
 router.use("/users", userRoutes);
 router.use("/", homeRoutes);
+router.use("/patients", patientRoutes);
 module.exports = router; 
